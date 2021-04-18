1b97c16aac8e7eb881fe68a7e55b4b0bc910eac5
mahesh-katla <mahesh143k@gmail.com>
2021-04-18 23:34:27 +0530

    wiremock

---
+++ src/main/java/com/example/demo/WiremockExampleApplication.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+@SpringBootApplication
+public class WiremockExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiremockExampleApplication.class, args);
+	}
+	
+	
+	 WireMockServer mWireMockServer;
+
+   	@Bean
+    public WireMockServer setup() {
+        mWireMockServer = new WireMockServer(8081);
+        mWireMockServer.start();
+        stubTest();
+        return mWireMockServer;
+    }
+    
+    public void stubTest() {
+    	mWireMockServer.stubFor(WireMock.get("/test")
+    			// .withHeader(HttpHeaders.ACCEPT,
+                        // WireMock.equalTo(MediaType.APPLICATION_JSON_VALUE))
+                         .willReturn(
+                                    WireMock.aResponse()
+                                            .withStatus(200)
+                                            .withHeader(HttpHeaders.CONTENT_TYPE, 
+                                 MediaType.APPLICATION_JSON_VALUE)
+                                            .withBodyFile("Test.json")
+                            )
+    			);
+    }
+
+}
